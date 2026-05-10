c7065b77de467194bb432da277ea50b4eb40ae97
rubelsec <rubelmrm1492@gmail.com>
2026-05-11 04:10:54 +0600

    add some barchart and spinner

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { useEffect,useState } from "react";
 import { BarChart, Bar, XAxis } from "recharts";
+import { Audio,Grid } from 'react-loader-spinner'
 
 
 const Phones = () => {
     const [phones, setPhones] = useState([]);
+    const [loading,setLoading]=useState(true);
     useEffect(()=>{
     //     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
     //     .then(res => res.json())
@@ -21,12 +23,27 @@ const Phones = () => {
         })
         console.log(phonesWithFakedata);
         setPhones(phonesWithFakedata);
+        setLoading(false);
 
     });
 },[]);
 
     return (
         <div>
+      { loading && <div>
+         <Audio
+          height={80}
+          width={80}
+          radius={9}
+          color="green"
+          ariaLabel="audio-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+    />
+    
+        </div>
+
+    }
             <h2 className="text-5xl">Phones: {phones.length}</h2>
              <BarChart width={1200} height={400} data={phones}>
     
